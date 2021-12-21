2edf623cffc685fb9f64942c7054846f5c5ba9de
docAlexxx <docalex@mail.ru>
2021-12-22 00:50:42 +0400

    Lesson1 init views

--- app/src/main/java/com/example/kotlinfirstlesson/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirstlesson/MainActivity.kt
@@ -3,8 +3,17 @@ package com.example.kotlinfirstlesson
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var buttonPrev: Button
+    lateinit var buttonNext: Button
+    lateinit var etKind: EditText
+    lateinit var etName: EditText
+    lateinit var etAge: EditText
+    var currentIndex = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,8 +29,15 @@ class MainActivity : AppCompatActivity() {
             Log.d(myTag, animalArray[i].kind + "'s name is " + animalArray[i].name)
         }
 
+        initViews()
 
     }
 
-
+    fun initViews() {
+        buttonPrev = findViewById(R.id.button_prev)
+        buttonNext = findViewById(R.id.button_next)
+        etKind = findViewById(R.id.et_animalKind)
+        etName = findViewById(R.id.et_animalName)
+        etAge = findViewById(R.id.et_animalAge)
+    }
 }
