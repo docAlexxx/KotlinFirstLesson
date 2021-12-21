841541b9e4999ea3c842aa9bff88e3617129b13a
docAlexxx <docalex@mail.ru>
2021-12-21 23:12:59 +0400

    Lesson1 made cycle to output to the console

--- app/src/main/java/com/example/kotlinfirstlesson/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirstlesson/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.kotlinfirstlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myTag: String = "myLog"
 
         val cat = Animal("Cat", 5, "Barsik")
         val dog = cat.copy("Dog", 7, "Tuzik")
         val horse = dog.copy("Horse", 15, "Kon")
 
-        val animalArray = arrayOf<Animal>(cat,dog,horse)
+        val animalArray = arrayOf<Animal>(cat, dog, horse)
+
+        for (i in 0 until animalArray.size) {
+            Log.d(myTag, animalArray[i].kind + "'s name is " + animalArray[i].name)
+        }
+
+
     }
 
 
