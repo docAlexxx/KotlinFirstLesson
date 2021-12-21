c433716e0ff01f77a895ebaa3509e2fb76f0c7bd
docAlexxx <docalex@mail.ru>
2021-12-21 22:47:03 +0400

    Lesson1 set objects

--- app/src/main/java/com/example/kotlinfirstlesson/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirstlesson/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        animalinit()
+
+
+    }
+
+    private fun animalinit() {
+        val cat = Animal("Cat", 5, "Barsik")
+        val dog = cat.copy("Dog", 7, "Tuzik")
+        val horse = dog.copy("Horse", 15, "Kon")
     }
 }
